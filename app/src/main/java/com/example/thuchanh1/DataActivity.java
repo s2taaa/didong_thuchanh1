b0f0088d59cc997ef1bfde0b3cdfe59fa811acd2
Your Name <you@example.com>
2021-05-28 14:07:38 +0700

    commit-cau2

--- app/src/main/java/com/example/thuchanh1/DataActivity.java
+++ app/src/main/java/com/example/thuchanh1/DataActivity.java
@@ -1,19 +1,95 @@
 package com.example.thuchanh1;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class DataActivity extends AppCompatActivity {
+    List<User> users = new ArrayList<>();
+    RecyclerView rcv_user;
+    UserAdapter adapter;
+    EditText edt_name,edt_email,edt_id;
+    Button btn_add,btn_update,btn_get;
+    String url = "https://60b094eb1f26610017ffe83c.mockapi.io/user";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.listview);
+        setContentView(R.layout.activity_user);
         getSupportActionBar().hide();
 
+        rcv_user = findViewById(R.id.rcv_users);
+        edt_name = findViewById(R.id.edit_name);
+        edt_email = findViewById(R.id.edit_email);
+        edt_id = findViewById(R.id.edit_id);
+
+        btn_add = findViewById(R.id.btn_add);
+        btn_update = findViewById(R.id.btn_update);
+        btn_get = findViewById(R.id.btn_get);
+
+
+        getJsonObjectArray(url, users);
+
+        adapter = new UserAdapter(users, this);
+        rcv_user.setAdapter(adapter);
+        rcv_user.setLayoutManager(new GridLayoutManager(this, 1));
+
+
     }
+
+
+
+        private void getJsonObjectArray(String url, List<User> users){
+            JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url,
+                    new Response.Listener<JSONArray>(){
+
+                        @Override
+                        public void onResponse(JSONArray response) {
+                            for(int i =0 ; i<response.length();i++){
+                                try {
+                                    JSONObject jsonObject = (JSONObject) response.get(i);
+                                    User user = new User();
+                                    user.setId(jsonObject.getString("id").toString());
+                                    user.setEmail(jsonObject.getString("age"));
+                                    user.setName(jsonObject.getString("name"));
+                                    users.add(user);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    },
+                    new Response.ErrorListener(){
+
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(DataActivity.this,
+                                    "Error by get Json Array!", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            requestQueue.add(jsonArrayRequest);
+        }
+
 
     }
---
+++ app/src/main/java/com/example/thuchanh1/UserAdapter.java
@@ -0,0 +1,54 @@
+package com.example.thuchanh1;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.NameViewHolder> {
+    private List<User> users;
+    private LayoutInflater inflater;
+
+    public UserAdapter(List<User> users, Context context) {
+        this.users = users;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public UserAdapter.NameViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+       View view = inflater.inflate(R.layout.line_item,parent,false);
+       return new NameViewHolder(view,this);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserAdapter.NameViewHolder holder, int position) {
+        User user = users.get(position);
+        holder.txt_id.setText("Id : "+user.getId());
+        holder.txt_email.setText("Age : " +user.getEmail());
+        holder.txt_name.setText("Name : "+user.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public class NameViewHolder extends RecyclerView.ViewHolder {
+        private UserAdapter adapter;
+        private TextView txt_id,txt_name,txt_email;
+        public NameViewHolder(@NonNull View itemView , UserAdapter adapter) {
+            super(itemView);
+            this.adapter = adapter;
+            txt_id = itemView.findViewById(R.id.txt_id);
+            txt_name = itemView.findViewById(R.id.txt_name);
+            txt_email = itemView.findViewById(R.id.txt_email);
+        }
+    }
+}
